9f733d6a66a52756ef725a58ced091f4368f4328
John Glen Siy <johnglen_siy@berkeley.edu>
2026-04-29 15:58:32 -0700

    add tennis to const SPORTS

--- tools/lib/getSport.ts
+++ tools/lib/getSport.ts
@@ -1,4 +1,10 @@
-export const SPORTS = ["basketball", "football", "baseball", "hockey"] as const;
+export const SPORTS = [
+	"basketball",
+	"football",
+	"baseball",
+	"hockey",
+	"tennis",
+] as const;
 export type Sport = (typeof SPORTS)[number];
 
 export const getSport = (): Sport => {
